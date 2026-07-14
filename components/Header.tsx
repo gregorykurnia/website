c9dcebbc8f1bd431e20824be65433ed16d632029
gregorykurnia <gregory.kurnia@gmail.com>
2026-07-14 14:56:12 +0700

    auto-save

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Header() {
   const [scrolled, setScrolled] = useState(false);
